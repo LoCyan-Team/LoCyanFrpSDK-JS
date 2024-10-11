2d2ecf0f359a5e532ca858ca6f86d6295e97b271
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-10-11 13:59:45 +0800

    [*] 注释未完成的 API 导入

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,25 @@
-import auth from './auth'
-import donate from './donate'
+// import auth from './auth'
+// import donate from './donate'
 import icp from './icp.api'
-import node from './node'
-import proxy from './proxy'
-import verification from './verification'
+// import node from './node'
+// import proxy from './proxy'
+// import verification from './verification'
 import sign from './sign.api'
-import user from './user'
-import email from './email'
-import notice from './notice'
-import minecraft from './minecraft'
+// import user from './user'
+// import email from './email'
+// import notice from './notice'
+// import minecraft from './minecraft'
 
 export default {
-  user: user,
-  auth: auth,
-  email: email,
-  verification: verification,
-  proxy: proxy,
-  node: node,
-  donate: donate,
+  // user: user,
+  // auth: auth,
+  // email: email,
+  // verification: verification,
+  // proxy: proxy,
+  // node: node,
+  // donate: donate,
   sign: sign,
   icp: icp,
-  notice: notice,
-  minecraft: minecraft
+  // notice: notice,
+  // minecraft: minecraft
 }
